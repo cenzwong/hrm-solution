37dba7ffc6b2f4c6d8c3bdb20f0a338a45307adc
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2024-09-13 11:19:08 +0100

    update

---
+++ 10. Octoplier Suite 9.36/9.36.asm
@@ -0,0 +1,10 @@
+a:
+    INBOX
+    COPYTO  0
+    ADD     0
+    COPYTO  0
+    ADD     0
+    COPYTO  0
+    ADD     0
+    OUTBOX
+    JUMP    a
---
+++ 11. Sub Hallway 10.40/11.44.asm
@@ -0,0 +1,12 @@
+a:
+    INBOX
+    COPYTO 0
+    INBOX
+    SUB 0
+    COPYTO 0
+    OUTBOX
+    COPYFROM 0
+    SUB 0
+    SUB 0
+    OUTBOX
+    JUMP a
