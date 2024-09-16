4178eed7c2b50e3fe161c287300ff3dff4684365
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2024-09-16 10:20:28 +0100

    udpate

--- 20. Multiplication Workshop/.asm
+++ 20. Multiplication Workshop/.asm
@@ -1,10 +1,24 @@
     JUMP        main
-
 out:
+    COPYFROM    c
+zout:
     OUTBOX
 main:
     INBOX
     JUMPZ       az
     COPYTO      a
     INBOX
-    JUMPZ       out     ;   output zero
+    JUMPZ       zout        ;   output zero
+    COPYTO      b
+    COPYTO      c
+mul:
+    BUMPDN      a
+    JUMPZ       out
+    COPYFROM    c
+    ADD         b
+    COPYTO      c
+    JUMP        mul
+az:
+    OUTBOX
+    INBOX                   ; skip this INBOX
+    JUMP        main
